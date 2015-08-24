e2a9df52d73a32574f3686c22ce42ddbe6bbfb2f
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-24 16:37:25 +0100

    Initial Commit

---
+++ FizzBuzz2Tests/FizzBuzz2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FizzBuzz2Tests.swift
+//  FizzBuzz2Tests
+//
+//  Created by Rebecca Appleyard on 24/08/2015.
+//  Copyright (c) 2015 Firefly. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FizzBuzz2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
